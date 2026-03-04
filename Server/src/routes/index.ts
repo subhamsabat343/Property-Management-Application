ad53a862f06c1b06882a1b879692c369b36efc1b
Subham Sabat <96640999+subhamsabat343@users.noreply.github.com>
2026-03-04 19:49:23 +0530

    fix: fix changes on server (#5)
    * fix: fix changes on server
    
    * fix: fix changes on server 2

--- Server/src/routes/index.ts
+++ Server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import propertyRoutes from "../modules/property/property.routes.ts";
+import propertyRoutes from "@/modules/property/property.routes.ts";
+import organizationRoutes from "@/modules/organizations/organization.routes.ts";
 
 const router = Router();
 
 router.use("/properties", propertyRoutes);
+router.use("/organizations", organizationRoutes);
 
 export default router;
